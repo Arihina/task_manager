f37f3fefe72e76952c9f507c71f4244bf512b135
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-12-01 16:28:35 +0300

    add getting tasks from json

--- manager/json_utilities.py
+++ manager/json_utilities.py
@@ -7,7 +7,27 @@ FILE_PATH = os.path.join(os.path.dirname(__file__), '../resources/tasks.json')
 
 
 class Reader:
-    pass
+    @staticmethod
+    def get_task_by_id(id: int) -> Task | None:
+        try:
+            with open(FILE_PATH, 'r', encoding='utf-8') as file:
+                tasks = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            raise FileNotFoundError('Не найден файл tasks.json в папке resources')
+
+        for task in tasks:
+            if task['id'] == id:
+                return task
+
+    @staticmethod
+    def get_tasks_by_category(category: str) -> list[Task]:
+        try:
+            with open(FILE_PATH, 'r', encoding='utf-8') as file:
+                tasks = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            raise FileNotFoundError('Не найден файл tasks.json в папке resources')
+
+        return [task for task in tasks if task['category'] == category]
 
 
 class Writer:
@@ -24,3 +44,7 @@ class Writer:
 
         with open(FILE_PATH, 'w', encoding='utf-8') as file:
             json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+    @staticmethod
+    def remove_task(id: int | None, category: str | None) -> None:
+        pass
